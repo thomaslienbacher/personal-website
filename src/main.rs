5117c3c8d9626ccd570b9e4c16cbf0dae6713385
Thomas Lienbacher <lienbacher.tom@gmail.com>
2022-06-07 22:55:34 +0200

    Removed TLS and updated dependencies TLS was removed because we use NGINX anyway and that takes care of TLS. Updated dependencies to their newest version and removed some.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
-use std::fs::File;
-use std::io::BufReader;
 use actix_files::Files;
 use actix_web::{middleware, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-use rustls::internal::pemfile::{certs, pkcs8_private_keys};
-use rustls::{NoClientAuth, ServerConfig};
-use dotenv::dotenv;
-
 
 async fn index(_req: HttpRequest) -> impl Responder {
     HttpResponse::Ok()
@@ -21,15 +15,6 @@ async fn robots(_req: HttpRequest) -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    dotenv().ok();
-
-    let cert_file_path = std::env::var("CERT_FILE").unwrap_or_else(|_| {
-        panic!("Please set CERT_FILE environment variable (fullchain.pem)!")
-    });
-    let key_file_path = std::env::var("KEY_FILE").unwrap_or_else(|_| {
-        panic!("Please set KEY_FILE environment variable (privkey.pem)!")
-    });
-
     if std::env::var("RUST_LOG").is_ok() {
         println!("WARNING: Logging is enabled!");
         env_logger::init();
@@ -37,19 +22,7 @@ async fn main() -> std::io::Result<()> {
         println!("For logging set RUST_LOG to actix_server=debug,actix_web=debug")
     }
 
-    // load ssl keys
-    let mut config = ServerConfig::new(NoClientAuth::new());
-    let cert_file = &mut BufReader::new(File::open(cert_file_path).unwrap());
-    let key_file = &mut BufReader::new(File::open(key_file_path).unwrap());
-    let cert_chain = certs(cert_file).unwrap();
-    let mut keys = pkcs8_private_keys(key_file).unwrap();
-    if keys.is_empty() {
-        eprintln!("Could not locate PKCS 8 private keys.");
-        std::process::exit(1);
-    }
-    config.set_single_cert(cert_chain, keys.remove(0)).unwrap();
-
-    println!("Starting https server: 0.0.0.0:443");
+    println!("Starting http server at localhost:8001");
 
     if std::env::var("RUST_LOG").is_ok() {
         HttpServer::new(|| {
@@ -59,7 +32,7 @@ async fn main() -> std::io::Result<()> {
                 .service(web::resource("/").route(web::get().to(index)))
                 .service(Files::new("/static", "static"))
         })
-            .bind_rustls("0.0.0.0:443", config)?
+            .bind("localhost:8001")?
             .run()
             .await
     } else {
@@ -69,7 +42,7 @@ async fn main() -> std::io::Result<()> {
                 .service(web::resource("/").route(web::get().to(index)))
                 .service(Files::new("/static", "static"))
         })
-            .bind_rustls("0.0.0.0:443", config)?
+            .bind("localhost:8001")?
             .run()
             .await
     }
